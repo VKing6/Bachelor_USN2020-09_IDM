d70a4d0306bb4234de99a14c0cad2a49541a3bcd
Kristian Bjørge Auestad <kauestad@gmail.com>
2020-04-21 14:27:22 +0200

    SerialReceiver now takes a serial object

--- IntComm/serialrcv.py
+++ IntComm/serialrcv.py
@@ -1,35 +1,33 @@
 #!/usr/bin/env python3
-import serial
 import threading
 import dataobject
+
 import time
+import serial
 
 class SerialReceiver(threading.Thread):
-    def __init__(self, data, stop_event,
-                 port="/dev/ttyACM0", rate=57600, timeout=1):
-        self.port = port
-        self.rate = rate
-        self.timeout = timeout
+    def __init__(self, serialport, data, stop_event):
+        self.ser = serialport
         self.data = data
         self.stop_event = stop_event
         threading.Thread.__init__(self)
 
     def run(self):
-        with serial.Serial(port=self.port, baudrate=self.rate, timeout=self.timeout) as ser:
-            while not self.stop_event.is_set():
-                in_data = ser.readline()
-                if len(in_data) > 0:
-                    self.data.parse_datastring(in_data)
+        while not self.stop_event.is_set():
+            in_data = self.ser.readline()
+            if len(in_data) > 0:
+                self.data.parse_datastring(in_data)
 
 if __name__ == "__main__":
     data = dataobject.DataObject()
     event = threading.Event()
-    rcv = SerialReceiver(data, event)
-    rcv.start()
-    print("Starting")
-    for i in range(4):
-        print(i, data.get_time())
-        time.sleep(1.5)
-    print("End")
-    event.set()
-    rcv.join()
+    with serial.Serial('/dev/ttyACM0', 57600, timeout=1) as ser:
+        rcv = SerialReceiver(ser, data, event)
+        rcv.start()
+        print("Starting")
+        for i in range(4):
+            print(i, data.get_time())
+            time.sleep(1.5)
+        print("End")
+        event.set()
+        rcv.join()
